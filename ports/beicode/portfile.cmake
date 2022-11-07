66952e774c940668d2af29cb64c741926c1410f6
Victor Romero <romerosanchezv@gmail.com>
2022-11-07 02:52:50 +0000

    Update ports upstreams

--- ports/beicode/portfile.cmake
+++ ports/beicode/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO northwindtraders/beicode
+	REPO vicroms/beicode
 	REF 19a1f95c2f56a27ced90227b5e2754a602a08e69
 	SHA512
 7b2bb7acb2a8ff07bff59cfa27247a7b2cced03828919cd65cc0c8cf1f724f5f1e947ed6992dcdbc913fb470694a52613d1861eaaadbf8903e94eb9cdfe4d000
--- ports/beison/portfile.cmake
+++ ports/beison/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
-	REPO northwindtraders/beison
+	REPO vicroms/beison
 	REF 1920a0411fc48f1e805f0001e6a0975865217b70
 	SHA512 a3ad804f00e3deecef16c76d5c8355a0a727579145de210def8be64b775ec247477ec58d01bbdc233d467626eed2caf7048c60c147c7177cf8bb5d59add0c99a
 	HEAD_REF main
